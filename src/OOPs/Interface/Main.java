8252817ec9e4f498b22a7ab5aee3a8b3515c1728
yellendhar08 <yellendharlodi225@gmail.com>
2026-01-09 11:08:39 +0530

    new commit2

--- src/OOPs/Interface/Main.java
+++ src/OOPs/Interface/Main.java
@@ -11,8 +11,26 @@ class Student extends Person{
 class Teacher extends Person{
 
 }
+interface  Sports{
+    abstract public void play();
+}
+interface Cultural{
+    abstract public void perform();
+}
+class CollegeStudent extends Student implements Sports, Cultural{
+    public void play(){
+        System.out.println("Sports method over ride");
+    }
+    public void perform(){
+        System.out.println("Cultural method over ride");
+    }
+}
 public class Main {
     public static void main(String[] args) {
+        CollegeStudent obj = new CollegeStudent();
+        obj.showRole();
+        obj.play();
+        obj.perform();
 
     }
 }
