0a1239c6213287033cc957c18d3d18bec2fed7f1
yellendhar08 <yellendharlodi225@gmail.com>
2026-01-21 12:00:37 +0530

    new commit2

--- src/RegularExpressions/RegularExpression.java
+++ src/RegularExpressions/RegularExpression.java
@@ -50,6 +50,35 @@ public class RegularExpression {
         System.out.println("123Suraj".matches(".*"));
         System.out.println("12345".matches(".*"));
 
+        System.out.println();
+        System.out.println("5".matches("\\d")); //exactly one digit
+        System.out.println("0".matches("\\d"));
+        System.out.println("9".matches("\\d"));
+        System.out.println("12".matches("\\d"));
+        System.out.println("a".matches("\\d"));
+        System.out.println(" ".matches("\\d"));
+
+        System.out.println();
+        System.out.println("a".matches("\\w"));
+        System.out.println("Z".matches("\\w"));
+        System.out.println("5".matches("\\w"));
+        System.out.println("_".matches("\\w"));
+        System.out.println("@".matches("\\w"));
+        System.out.println(" ".matches("\\w"));
+
+        System.out.println();
+        System.out.println(" ".matches("\\s"));
+        System.out.println("\t".matches("\\s"));
+        System.out.println("\n".matches("\\s"));
+        System.out.println("a".matches("\\s"));
+        System.out.println("1".matches("\\s"));
+
+
+
+
+
+
+
 
 
     }
